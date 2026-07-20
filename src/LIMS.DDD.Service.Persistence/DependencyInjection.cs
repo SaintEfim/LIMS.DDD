406be841cddf654968de84062ea6b8bb940056b0
SaintEfim <saintefim2@gmai.com>
2026-07-20 21:11:38 +0300

    fix dependencies injection

--- src/LIMS.DDD.Service.Persistence/DependencyInjection.cs
+++ src/LIMS.DDD.Service.Persistence/DependencyInjection.cs
@@ -1,5 +1,4 @@
-﻿using System.Reflection;
-using LIMS.DDD.Service.Domain.StudyTemplateAggregate;
+﻿using LIMS.DDD.Service.Domain.StudyTemplateAggregate;
 using LIMS.DDD.Service.Persistence.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -13,8 +12,6 @@ public static class DependencyInjection
         this IServiceCollection services,
         IConfiguration configuration)
     {
-        var currentAssembly = Assembly.GetExecutingAssembly();
-
         services.AddScoped<IStudyTemplateRepository, StudyTemplateRepository>();
 
         services.AddDbContext<ApplicationDbContext>(options =>
