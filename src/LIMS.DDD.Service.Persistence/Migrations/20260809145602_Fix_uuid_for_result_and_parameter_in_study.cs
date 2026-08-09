720569c256d06abf452b9d61a20805ec5536cb17
SaintEfim <saintefim2@gmai.com>
2026-08-09 17:56:22 +0300

    added migration

---
+++ src/LIMS.DDD.Service.Persistence/Migrations/20260809145602_Fix_uuid_for_result_and_parameter_in_study.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMS.DDD.Service.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Fix_uuid_for_result_and_parameter_in_study : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
