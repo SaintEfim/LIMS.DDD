cfef80f5f7e9aed882741946987f881e62b21838
SaintEfim <saintefim2@gmail.com>
2026-08-19 11:02:32 +0300

    added migration guid methodokigies

--- src/Guides.Service/Migrations/20260819080002_added_index_for_unit.Designer.cs
+++ src/Guides.Service/Migrations/20260819080002_added_index_for_unit.Designer.cs
@@ -3,17 +3,20 @@ using System;
 using Guides.Service.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
 #nullable disable
 
-namespace Guides.Service.Persistence.Migrations
+namespace Guides.Service.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260819080002_added_index_for_unit")]
+    partial class added_index_for_unit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Guides.Service/Migrations/20260819080002_added_index_for_unit.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Guides.Service.Migrations
+{
+    /// <inheritdoc />
+    public partial class added_index_for_unit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Units_Name",
+                table: "Units",
+                column: "Name",
+                unique: true,
+                filter: "\"IsDeleted\" = false");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Units_Name",
+                table: "Units");
+        }
+    }
+}
