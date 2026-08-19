04fb83db30c505309c17aec6e3f20b8e991d891f
SaintEfim <saintefim2@gmail.com>
2026-08-19 10:45:21 +0300

    added unitDto

---
+++ src/LIMS.Service.LaboratoryOperations.Application/Units/UnitDto.cs
@@ -0,0 +1,12 @@
+﻿using LIMS.Service.LaboratoryOperations.Domain.UnitSnapshots;
+
+namespace LIMS.Service.LaboratoryOperations.Application.Units;
+
+public sealed record UnitDto(Guid Id, string Name)
+{
+    public static UnitDto FromSnapshot(
+        UnitSnapshot snapshot)
+    {
+        return new UnitDto(snapshot.Id.Value, snapshot.Name.Value);
+    }
+}
