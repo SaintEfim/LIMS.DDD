6a3c62c24f4c1b30fd5ab3c634958fb71b52e3ad
saintefim <saintefim2@gmail.com>
2026-09-06 22:20:12 +0300

    added migration

---
+++ src/LIMS.Service.Methodologies.Persistence/Migrations/20260906191927_Make_not_deleteble_unit.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LIMS.Service.Methodologies.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Make_not_deleteble_unit : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DeletedAt",
+                table: "UnitSnapshots");
+
+            migrationBuilder.DropColumn(
+                name: "IsDeleted",
+                table: "UnitSnapshots");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTimeOffset>(
+                name: "DeletedAt",
+                table: "UnitSnapshots",
+                type: "timestamp with time zone",
+                nullable: true);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsDeleted",
+                table: "UnitSnapshots",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
