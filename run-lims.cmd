edc48119214dc9d82c98621770af1d007c6c9359
saintefim <saintefim2@gmail.com>
2026-09-11 21:29:45 +0300

    bugfix/fixe_readm_file_and_script

--- run-lims.cmd
+++ run-lims.cmd
@@ -39,7 +39,7 @@ if "%USE_WT%"=="1" (
 )
 echo.
 
-call :run_service Guides.Service                        Guides.Service                        "Units [Guides]"
+call :run_service Guides.Service.API                     Guides.Service.API                     "Units [Guides]"
 call :run_service LIMS.Service.Methodologies.API        LIMS.Service.Methodologies.API        "Methodologies"
 call :run_service LIMS.Service.LaboratoryOperations.API LIMS.Service.LaboratoryOperations.API "Laboratory Operations"
 
