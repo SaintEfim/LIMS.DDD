3ec43b343189017f7fb4a5439b8cf39d8d546e7f
SaintEfim <saintefim2@gmail.com>
2026-09-20 17:28:30 +0300

    added new dto, notificationDto

--- src/LIMS.Service.LaboratoryOperations.API/Hubs/NotificationHub.cs
+++ src/LIMS.Service.LaboratoryOperations.API/Hubs/NotificationHub.cs
@@ -2,11 +2,21 @@
 
 namespace LIMS.Service.LaboratoryOperations.API.Hubs;
 
+public class NotificationHubDto
+{
+    public Guid UserId { get; set; }
+
+    public string NameOperation { get; set; } = string.Empty;
+
+    public string Result { get; set; } = string.Empty;
+}
+
 public class NotificationHub : Hub
 {
+    // Надо писать в определённый hub
     public async Task Send(
-        string message)
+        NotificationHubDto messageModel)
     {
-        await Clients.All.SendAsync("Receive", message);
+        await Clients.All.SendAsync("Receive", messageModel);
     }
 }
